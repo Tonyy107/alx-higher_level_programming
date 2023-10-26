a4336df2a0d88499c2578bb71f0fd9c06d28fef9
Tonyy107 <antonmagdy53@gmail.com>
2023-10-26 22:41:41 +0300

    lol

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,10 +5,10 @@ class base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        #this is init method
         self.id = id
         if id is not None:
             self.id = id
         else:
              base.__nb_objects += 1
              self.id = base.__nb_objects 
-             
