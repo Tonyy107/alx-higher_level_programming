7ab0a9ed065ee3ac0a30d7331492344a04aebde9
Tonyy107 <antonmagdy53@gmail.com>
2023-10-26 23:39:31 +0200

    lol

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+"""
+rectangle class
+"""
+from base import Base
+
+
+class Rectangle(Base):
+    """
+    Rectangle class
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        init - initialization
+        Args:
+            width: it just a width
+            height: it just a height
+            x: it equal to 0
+            y: it equal to 0 too
+            id: objects id
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """
+        width getter
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """
+        width setter
+        """
+        self.__width = width
+
+    @property
+    def height(self):
+        """
+        height getter
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """
+        height setter
+        """
+        self.__height = height
+
+    @property
+    def x(self):
+        """
+        x getter
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """
+        x setter
+        """
+        self.__x = x
+
+    @property
+    def y(self):
+        """
+        y getter
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """
+        y setter
+        """
+        self.__y = y
