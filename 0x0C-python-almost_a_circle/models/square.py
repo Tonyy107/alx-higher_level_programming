0a85a4c1d7243ffb613b53e1a15557cc28b183e5
Tonyy107 <antonmagdy53@gmail.com>
2023-10-27 19:39:15 +0200

    lol

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,8 +4,18 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    Square class
+    """
     def __init__(self, size, x=0, y=0, id=None):
-
+        """
+        init - initialization
+        Args:
+            size: it's size of square dude!!!
+            x: the same x as before
+            y: the same y as before
+            id: the same id as before
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
