b58907b673be02537846eaf527eee8665d99b0c6
Tonyy107 <antonmagdy53@gmail.com>
2023-10-28 17:00:55 +0200

    lol

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
- base this is base class
-"""
+"""base module"""
 
 
 class Base:
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
-"""
-rectangle class
-"""
+"""rectangle module"""
 from models.base import Base
 
 
