0dfba8d62670060bdb407cad4fa413583a58625c
Tonyy107 <antonmagdy53@gmail.com>
2023-11-20 02:05:06 +0200

    Refactor code for improved readability and formatting

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -15,19 +15,21 @@ from relationship_city import City
 if __name__ == "__main__":
     # Check if all required command line arguments are provided.
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
         sys.exit(1)
 
     # Create a database engine using command line arguments.
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    
+
     # Create a session maker and establish a session with the database.
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query and list all City objects ordered by their IDs, including State information.
+    # Query and list all City objects ordered by their IDs,
+    # including State information.
     cities = session.query(City).order_by(City.id).all()
 
     for city in cities:
