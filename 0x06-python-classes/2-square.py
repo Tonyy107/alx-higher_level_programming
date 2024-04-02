982edbb10cb1990f7cb3c99da2e4b7f10461463f
Tonyy107 <antonmagdy53@gmail.com>
2024-04-02 22:49:35 +0200

    Fix spacing issue in Square class constructor

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,10 +4,10 @@
 
 class Square:
     "square class"
-    def __init__(self, size=0):
+    def __init__(self, size = 0):
         if type(size) != int:
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
         else:
-            self. _Square__size = size
+            self._Square__size = size
