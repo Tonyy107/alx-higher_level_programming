1a643b127c775d69e0eaf59a548644d7f6b1a447
Tonyy107 <antonmagdy53@gmail.com>
2024-04-06 16:19:28 +0200

    Refactor Square class initialization and add error handling

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-""" defines a square """
+""" defines a class named Square """
 
 
 class Square:
-    """ square with private instance attribute size """
-
+    """ defines a function named __init__ """
     def __init__(self, size=0):
-        """
-        Args:
-            size: size of square
-        """
+        """ if statement """
         if type(size) != int:
-            raise TypeError('size must be an integer')
+            """ raise an error """
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError('size must be >= 0')
+            """ raise an error """
+            raise ValueError("size must be >= 0")
         else:
+            """ initialize __size of self with size """
             self.__size = size
