91d4b0969eda1f209a307013090e650a3b0ad199
Tonyy107 <antonmagdy53@gmail.com>
2024-04-06 19:55:57 +0200

    Update Square class instantiation and add my_print method

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" defines a class named Square """
+
+
+class Square:
+    """ defines a function named __init__ """
+    def __init__(self, size=0):
+        """ init """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        a = isinstance(value, int)
+        if a:
+            self.__size = value
+            if value < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ area method """
+        return (self.__size*self.__size)
+
+    def my_print(self):
+        """ print method """
+        for a in range(self.__size):
+            for e in range(self.__size):
+                print("#",end="")
+            print("\n",end="")
