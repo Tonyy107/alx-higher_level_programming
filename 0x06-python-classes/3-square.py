d0b8d31963213f18dbe2ba3fed3d221968ff0416
Tonyy107 <antonmagdy53@gmail.com>
2024-04-06 17:03:21 +0200

    Update Square class instantiation and add area calculation

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" defines a class named Square """
+
+
+class Square:
+    """ defines a function named __init__ """
+    def __init__(self, size=0):
+        """ if statement """
+        a = isinstance(size, int)
+        if a:
+            self.__size = size
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return (self.__size*self.__size)
