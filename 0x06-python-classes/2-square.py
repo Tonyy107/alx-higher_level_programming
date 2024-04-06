3333ce34cfdc0ff2ee01c3ed1f17655148a4940f
Tonyy107 <antonmagdy53@gmail.com>
2024-04-06 16:31:13 +0200

    Fix isinstance() comparison in Square class constructor

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """ defines a function named __init__ """
     def __init__(self, size=0):
         """ if statement """
-        a = isinstance(size,int)
+        a = isinstance(size, int)
         if a == False:
             """ raise an error """
             raise TypeError("size must be an integer")
