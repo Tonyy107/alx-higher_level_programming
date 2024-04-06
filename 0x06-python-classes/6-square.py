580696f91b075f47c33e8c11d7a4ec1d7425b83b
Tonyy107 <antonmagdy53@gmail.com>
2024-04-06 20:45:02 +0200

    lol

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" defines a class named Square """
+
+
+class Square:
+    """ defines a function named __init__ """
+    def __init__(self, size=0, position=(0, 0)):
+        """ init """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        a = isinstance(value, int)
+        if a:
+            self.__size = value
+            if value < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        pass
+
+    def area(self):
+        """ area method """
+        return (self.__size*self.__size)
+
+    def my_print(self):
+        """ print method """
+        if self.__size == 0:
+            print("")
+        else:
+            for a in range(self.__size):
+                for e in range(self.__size):
+                    print("#", end="")
+                print("\n", end="")
