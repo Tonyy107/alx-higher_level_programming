5b569366413fb5d87695a41a816e7bf9ce5c2336
Tonyy107 <antonmagdy53@gmail.com>
2024-04-08 21:21:36 +0200

    Refactor Square class to use list comprehension for printing the square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -34,6 +34,4 @@ class Square:
             print("")
         else:
             for a in range(self.__size):
-                for e in range(self.__size):
-                    print("#", end="")
-                print("\n", end="")
+                print("".join("#" for z in range(self.__size)))
