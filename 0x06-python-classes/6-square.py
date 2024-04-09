e94e4317ef78f8741bcddc55907d30f4d865fe10
Tonyy107 <antonmagdy53@gmail.com>
2024-04-09 17:26:45 +0200

    Refactor position validation in Square class to improve type checking and handle invalid input

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,7 +31,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-       
+
         if type(value) is not tuple or len(value) != 2 or \
            type(value[0]) is not int or value[0] < 0 or \
            type(value[1]) is not int or value[1] < 0:
