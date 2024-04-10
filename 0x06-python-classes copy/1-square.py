0ff2d475ad21669a2dc021d642b250edd1ab77a8
Tonyy107 <antonmagdy53@gmail.com>
2024-04-10 13:47:04 +0200

    Refactor Square class to remove unnecessary code and improve readability

--- 0x06-python-classes copy/1-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"this is square class"
-
-
-class Square:
-    "square class"
-    def __init__(self, size):
-        self._Square__size = size
--- 0x06-python-classes copy/README.md
+++
@@ -1,9 +0,0 @@
-# python-classes
-## tasks
-* 0-square.py
-* 1-square.py
-* 2-square.py
-* 3-square.py
-* 4-square.py
-## main file
-* main.py
