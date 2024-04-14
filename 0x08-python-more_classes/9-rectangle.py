76bcd39cbcb3f606bd413e218c73e057d9ebf3f7
Tonyy107 <antonmagdy53@gmail.com>
2024-04-14 16:17:40 +0200

    Fix type check in width and height setters of Rectangle class

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ width setter """
-        if type(value) is not int:
+        if isinstance(value, int) is not True:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -32,7 +32,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ height setter """
-        if type(value) is not int:
+        if isinstance(value, int) is not True:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
