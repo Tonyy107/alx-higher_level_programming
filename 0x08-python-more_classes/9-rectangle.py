92ed09628be991760b6ec56166950d904d4e69ac
Tonyy107 <antonmagdy53@gmail.com>
2024-04-14 16:30:01 +0200

    Fix type check in width and height setters of Rectangle class

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         type(self).number_of_instances += 1
-        self.height = height
-        self.width = width
+        self.__height = height
+        self.__width = width
 
     @property
     def width(self):
