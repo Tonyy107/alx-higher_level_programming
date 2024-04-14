c46dbeafc200c22e6dd24585eff36c154d022916
Tonyy107 <antonmagdy53@gmail.com>
2024-04-14 15:36:57 +0200

    Refactor Rectangle class to fix square method implementation

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -48,10 +48,10 @@ class Rectangle:
             return rect_1
         else:
             return rect_2
-        
+
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        return (cls(size, size))
 
     def area(self):
         return self.__height*self.__width
