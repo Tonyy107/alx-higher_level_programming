9291c190c83f83c2337712b81c0eb131ec3a8e93
Tonyy107 <antonmagdy53@gmail.com>
2024-04-15 19:15:17 +0200

    Add print_sorted method to MyList class in 0x0A-python-inheritance/1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """ 1-my_list module """
 
+
 class MyList(list):
     """Mylist class"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
     def print_sorted(self):
-        """print_sorted function"""
-        pass
+        """print_sorted function thats print lists in sorted form 0 to more"""
+        print(sorted(self))
