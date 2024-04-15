f19a6ef5162c318f179810ef1b88d9bf580fcc7e
Tonyy107 <antonmagdy53@gmail.com>
2024-04-15 18:25:14 +0200

    Fix indentation in add_integer function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" 0-add_integer module"""
+
+
 def add_integer(a, b=98):
+    """add_integer function"""
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
