cd11cd8e4b8cfbac6e9c829564fd169e89b0ea02
Tonyy107 <antonmagdy53@gmail.com>
2024-04-22 15:19:32 +0200

    Refactor add_integer function to use type() instead of isinstance() for type checking

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,9 +4,9 @@
 
 def add_integer(a, b=98):
     """add_integer function"""
-    if ((not isinstance(a, int) and not isinstance(a, float))):
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
