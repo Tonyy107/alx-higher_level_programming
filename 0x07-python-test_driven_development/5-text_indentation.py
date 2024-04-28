6d852ac6589a3d1ca83730cd3f4c10374162c505
Tonyy107 <antonmagdy53@gmail.com>
2024-04-28 19:16:45 +0300

    Refactor main.py and add text_indentation function

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Text indentation module """
+
+
+def text_indentation(text):
+    """  prints a text with 2 new lines after each of these characters: ., ? and : """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    x = 0
+    while x < len(text):
+        print(text[x], end="")
+        if text[x] in ".?:":
+            x += 1
+            print("\n")
+
+        x += 1
