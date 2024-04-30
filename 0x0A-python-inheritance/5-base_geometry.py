04b16f4735e3c30572663c37e1cb8f404c5ea18c
Tonyy107 <antonmagdy53@gmail.com>
2024-04-30 12:20:59 +0300

    Add docstring to BaseGeometry class in 5-base_geometry.py

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -3,4 +3,13 @@
 
 
 class BaseGeometry:
-    pass
+     
+     """
+    A base class for geometric shapes.
+
+    This class provides common functionality and properties that are shared among different geometric shapes.
+    Subclasses are expected to implement specific methods for their respective shapes.
+
+    Attributes:
+    None
+    """
