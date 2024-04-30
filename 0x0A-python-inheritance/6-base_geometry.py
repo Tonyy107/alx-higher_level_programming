d472962b1415abc4c705df14a00882cb927a234d
Tonyy107 <antonmagdy53@gmail.com>
2024-04-30 12:26:49 +0300

    lol

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 5-base_geometry module """
+
+
+class BaseGeometry:
+
+    """
+    A base class for geometric shapes.
+
+    This class provides common functionality and properties
+    that are shared among different geometric shapes.
+    Subclasses are expected to implement specific methods
+     for their respective shapes.
+
+    Attributes:
+    None
+
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
