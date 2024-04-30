5cbf252abd3e5adb7d966100cbb2bf5974739445
Tonyy107 <antonmagdy53@gmail.com>
2024-04-30 12:14:19 +0300

    Refactor inherits_from function to improve readability in 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,8 @@
 
 def inherits_from(obj, a_class):
     """
-    Returns True if the object is an instance of a class that inherited 
-    (directly or indirectly) from the specified class; otherwise False.
+    Returns True if the object is an instance of a class that
+    inherited(directly or indirectly) from the specified
+    class; otherwise False.
     """
     return isinstance(obj, a_class) and not obj.__class__ is a_class
