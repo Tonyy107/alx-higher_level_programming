9dc45abbd2c14a7f208fb96de6c737bc6e0a549e
Tonyy107 <antonmagdy53@gmail.com>
2024-04-30 12:21:46 +0300

    Update docstring in BaseGeometry class in 5-base_geometry.py

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -3,13 +3,13 @@
 
 
 class BaseGeometry:
-     
-     """
-    A base class for geometric shapes.
 
-    This class provides common functionality and properties that are shared among different geometric shapes.
-    Subclasses are expected to implement specific methods for their respective shapes.
-
-    Attributes:
-    None
     """
+   A base class for geometric shapes.
+
+   This class provides common functionality and properties that are shared among different geometric shapes.
+   Subclasses are expected to implement specific methods for their respective shapes.
+
+   Attributes:
+   None
+   """
