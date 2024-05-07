785c464e26568e4df0afc267db2a62849c5b6c93
Tonyy107 <antonmagdy53@gmail.com>
2024-05-07 20:22:36 +0300

    Update main.py to use Square instead of Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" this module contain Square class """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a square shape.
+
+    Attributes:
+        size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new Square instance with the given size.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square object.
+
+        Args:
+            size (int): The size of the square.
+
+        Returns:
+            None
+        """
+        self.__size = size
+        super().__init__(size, size)
