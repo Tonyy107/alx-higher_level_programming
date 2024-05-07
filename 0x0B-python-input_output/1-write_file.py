39ba0c0c7e7f9477a14cca6157796e0b3a33cbab
Tonyy107 <antonmagdy53@gmail.com>
2024-05-07 21:23:24 +0300

    chore: Update write_file function to include docstring and remove unnecessary code

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,16 @@
 
 
 def write_file(filename="", text=""):
+    """
+    Writes the given text to a file.
+
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The text to write to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
     with open(filename, 'w', encoding="UTF8") as file:
         file.write(text)
     return len(text)
