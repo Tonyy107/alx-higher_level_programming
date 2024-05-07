ffb50d14ed33ecd1a417df4b1f7c939f57245cf4
Tonyy107 <antonmagdy53@gmail.com>
2024-05-07 21:09:22 +0300

    Refactor code to remove unused JSON-related functions

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" this module have read_file function """
+
+
+def read_file(filename=""):
+    """
+    Reads and prints the contents of a file.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        None
+    """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
