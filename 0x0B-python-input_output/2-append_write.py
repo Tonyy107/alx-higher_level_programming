8accf9d5d9d0ba576514a1d51d1369622622bf9e
Tonyy107 <antonmagdy53@gmail.com>
2024-05-08 01:36:54 +0300

    Refactor append_write function to include docstring and improve code readability

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
-
+""" this module contain append_write function """
 
 
 def append_write(filename="", text=""):
+    """
+    Appends the given text to the end of the specified file.
+
+    Args:
+        filename (str): The name of the file to append the text to.
+        text (str): The text to be appended to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
     with open(filename, 'a', encoding="UTF8") as file:
         return file.write(text)
