c66cbe409294f942c02b51402a8ef8f85bc0d33b
Tonyy107 <antonmagdy53@gmail.com>
2024-05-09 14:36:07 +0300

    Refactor load_from_json_file function to include docstring and improve code readability

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,7 +2,21 @@
 """ this module contain load_from_json_file function """
 import json
 
+
 def load_from_json_file(filename):
-   
+    """
+    Load data from a JSON file and return the deserialized object.
+
+    Args:
+        filename (str): The name of the JSON file to load.
+
+    Returns:
+        The deserialized object from the JSON file.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        JSONDecodeError: If the JSON file is not valid.
+
+    """
     with open(filename, 'r') as file:
         return json.load(file)
