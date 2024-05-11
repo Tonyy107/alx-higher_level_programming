59c138d048110c64a02f7996e213805fccd80546
Tonyy107 <antonmagdy53@gmail.com>
2024-05-12 01:08:07 +0300

    Refactor add_item.py script to use extend instead of append for adding command line arguments

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,13 +7,10 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 
-try:
-    b = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    b = []
-
 if os.path.exists("add_item.json"):
     b = load_from_json_file("add_item.json")
+else:
+    b = []
 
 a = sys.argv[1:]
 b.extend(a)
