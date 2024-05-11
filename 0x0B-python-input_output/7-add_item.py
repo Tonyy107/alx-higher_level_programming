52a061fda8e39662d5dbe448d31054ac6a374908
Tonyy107 <antonmagdy53@gmail.com>
2024-05-11 22:22:15 +0300

    chore: Add argument to add_item.py script for adding items to JSON file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,7 @@ import sys
 
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-
-argss = sys.argv[1:]
+argss = []
+argss.append = sys.argv[1:]
 
 save_to_json_file(argss, "add_item.json")
