a156dc7eb43eb01e65c57fd6719c9c791cd3f207
Tonyy107 <antonmagdy53@gmail.com>
2024-07-12 02:07:29 +0300

    Refactor Base class to include to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,7 +24,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
+            
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Convert a list of dictionaries to a JSON string representation.
